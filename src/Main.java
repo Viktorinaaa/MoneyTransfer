65e175a733ba876177cd58af486fac05319fb55b
Viki <masalikavika@yandex.ru>
2021-10-11 16:39:09 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceCA = 2_000_000_000;
+        int creditTransferSum = 500_000_000;
+        int total = balanceCA + creditTransferSum;
+        System.out.println(total);
+    }
+}
+
